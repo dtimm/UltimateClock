644821ba4f50fc6ea8ee239daf778a25610cd0b8
David Timm <timmmeister@gmail.com>
2012-01-24 16:15:22 -0700

    not much happened here

--- src/com/armes/ultimate/UltimateClockAppWidgetProvider.java
+++ src/com/armes/ultimate/UltimateClockAppWidgetProvider.java
@@ -7,23 +7,33 @@ import android.widget.RemoteViews;
 
 public class UltimateClockAppWidgetProvider extends AppWidgetProvider
 {
-	 public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
-	 {
-		 timer = new CustomClock();
+	public void onEnabled(Context context)
+	{
+		timer.update();
 		 
-		 final int N = appWidgetIds.length;
-		 for (int i=0; i<N; i++)
-		 {
-	            int appWidgetId = appWidgetIds[i];
-	            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.ultimate_clock_appwidget);
-	            
-	            // Set text to the current CustomClock time.
-	            views.setTextViewText(R.id.currentTime, timer.toString());
-	            
-	            // Update the app widget.
-	            appWidgetManager.updateAppWidget(appWidgetId, views);
-		 }
-	 }
+		RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.ultimate_clock_appwidget);
+	           
+	    // Set text to the current CustomClock time.
+	    views.setTextViewText(R.id.currentTime, timer.toString());
+	}
+	
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
+	{
+		timer = new CustomClock();
+		 
+		final int N = appWidgetIds.length;
+		for (int i=0; i<N; i++)
+		{
+	           int appWidgetId = appWidgetIds[i];
+	           RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.ultimate_clock_appwidget);
+	           
+	           // Set text to the current CustomClock time.
+	           views.setTextViewText(R.id.currentTime, timer.toString());
+	           
+	           // Update the app widget.
+	           appWidgetManager.updateAppWidget(appWidgetId, views);
+		}
+	}
 	 
-	 CustomClock timer;
+	CustomClock timer;
 }
